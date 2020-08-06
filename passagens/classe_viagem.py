0ed94155125a630b9e037e43a5cc08e51248056b
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-05 23:12:04 -0300

    install and add in project django-widget-tweaks

---
+++ passagens/classe_viagem.py
@@ -0,0 +1,2 @@
+tipos_de_classes = {(1, "Econômica"), (2, "Executiva"), (3, "Primeira Classe")}
+
--- passagens/forms.py
+++ passagens/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from passagens.classe_viagem import tipos_de_classes
 
 
 class PassagemForms(forms.Form):
@@ -7,4 +9,14 @@ class PassagemForms(forms.Form):
     destino = forms.CharField(label="Destino", max_length=100)
     data_ida = forms.DateField(label="Ida", widget=DatePicker())
     data_volta = forms.DateField(label="Volta", widget=DatePicker())
-
+    data_pesquisa = forms.DateField(
+        label="Data da pesquisa", disabled=True, initial=datetime.today
+    )
+    classe_viagem = forms.ChoiceField(label="Classe do vôo", choices=tipos_de_classes)
+    informacoes = forms.CharField(
+        label="Informações extras",
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False,
+    )
+    email = forms.EmailField(label="Email", max_length=200)
