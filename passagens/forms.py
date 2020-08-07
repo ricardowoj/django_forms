a6b830404b632eb0b96b73822f87ed8a50f45284
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-06 23:15:51 -0300

    refactor forms passagens  with models

--- passagens/forms.py
+++ passagens/forms.py
@@ -3,24 +3,24 @@ from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classes
 from passagens.validation import *
+from passagens.models import *
 
 
-class PassagemForms(forms.Form):
-    origem = forms.CharField(label="Origem", max_length=100)
-    destino = forms.CharField(label="Destino", max_length=100)
-    data_ida = forms.DateField(label="Ida", widget=DatePicker())
-    data_volta = forms.DateField(label="Volta", widget=DatePicker())
+class PassagemForms(forms.ModelForm):
     data_pesquisa = forms.DateField(
         label="Data da pesquisa", disabled=True, initial=datetime.today
     )
-    classe_viagem = forms.ChoiceField(label="Classe do vôo", choices=tipos_de_classes)
-    informacoes = forms.CharField(
-        label="Informações extras",
-        max_length=200,
-        widget=forms.Textarea(),
-        required=False,
-    )
-    email = forms.EmailField(label="Email", max_length=200)
+
+    class Meta:
+        model = Passagem
+        fields = "__all__"
+        labels = {
+            "data_ida": "Data de ida",
+            "data_volta": "Data de volta",
+            "informacoes": "Informações",
+            "classe_viagem": "Classe do vôo",
+        }
+        widgets = {"data_ida": DatePicker(), "data_volta": DatePicker()}
 
     def clean(self):
         origem = self.cleaned_data.get("origem")
--- passagens/validation.py
+++ passagens/validation.py
@@ -22,4 +22,3 @@ def data_ida_menor_que_data_hoje(data_ida, data_pesquisa, lista_de_erros):
     """Verifica se data de ida é menor que data de hoje"""
     if data_ida < data_pesquisa:
         lista_de_erros["data_ida"] = "Data de ida não pode ser menor que hoje"
-
