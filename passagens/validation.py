a1ece01abb9b26f6197e54a7862facb820b623d7
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-06 22:28:58 -0300

    create more valdation in forms.py

--- passagens/validation.py
+++ passagens/validation.py
@@ -9,3 +9,17 @@ def campo_possui_numero(valor_campo, nome_campo, lista_de_erros):
     if any(char.isdigit() for char in valor_campo):
         lista_de_erros[nome_campo] = "Não inclua números neste campo"
 
+
+def data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros):
+    """Verifica se data de ida é maior que data de volta"""
+    if data_ida > data_volta:
+        lista_de_erros[
+            "data_volta"
+        ] = "Data de ida não pode ser maior que data de volta"
+
+
+def data_ida_menor_que_data_hoje(data_ida, data_pesquisa, lista_de_erros):
+    """Verifica se data de ida é menor que data de hoje"""
+    if data_ida < data_pesquisa:
+        lista_de_erros["data_ida"] = "Data de ida não pode ser menor que hoje"
+
