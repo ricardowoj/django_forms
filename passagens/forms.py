ce82202d0549c730cb39557fdd57f56fc6cb1481
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-06 23:29:33 -0300

    create pessoa in forms

--- passagens/forms.py
+++ passagens/forms.py
@@ -39,3 +39,10 @@ class PassagemForms(forms.ModelForm):
                 mensagem_erro = lista_de_erros[erro]
                 self.add_error(erro, mensagem_erro)
         return self.cleaned_data
+
+
+class PessoaForms(forms.ModelForm):
+    class Meta:
+        model = Pessoa
+        exclude = ["nome"]
+
